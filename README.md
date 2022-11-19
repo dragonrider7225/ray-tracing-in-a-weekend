6e3dc50c6403c1adc8c1d2d9759cb8209bcd79c3
Kevin Moonen <kevin.moonen99@gmail.com>
2022-11-19 17:02:45 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ray-tracing-in-a-weekend
+A Rust implementation of [Ray Tracing in One Weekend](https://raytracing.github.io/).
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//! The user interaction layer.
+
+#![warn(clippy::all)]
+#![warn(missing_copy_implementations, missing_docs, rust_2018_idioms)]
+#![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
+
+fn main() {
+    println!("Hello, world!");
+    todo!("Fix crate name in Cargo.toml");
+}
